d7d09e4ecb523db5ea96bfd0adb03673a15a7521
Furvent <furventt@gmail.com>
2021-06-21 11:16:10 +0200

    Clean service StatusHistory

--- src/main/java/fr/eql/ticketing/service/StatusHistoryServiceImpl.java
+++ src/main/java/fr/eql/ticketing/service/StatusHistoryServiceImpl.java
@@ -1,13 +1,11 @@
 package fr.eql.ticketing.service;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import fr.eql.ticketing.entity.StatusHistory;
-import fr.eql.ticketing.entity.Ticket;
 import fr.eql.ticketing.repository.StatusHistoryRepository;
 
 @Service
@@ -29,47 +27,9 @@ public class StatusHistoryServiceImpl implements StatusHistoryService {
 	}
 
 	@Override
-	public List<StatusHistory> getAllStatusHistories() {
-		return repository.findAll();
-	}
-
-	@Override
 	public StatusHistory getStatusHistoryById(Long logId) {
-		return repository.findById(logId).get();
-	}
-
-	@Override
-	public void delete(StatusHistory statusHistory) {
-		repository.delete(statusHistory);
-	}
-
-	@Override
-	public StatusHistory update(StatusHistory statusHistory) {
-		return repository.save(statusHistory);
-	}
-	
-	@Override
-	public List<StatusHistory> getStatusHistoriesFromThisTicket(Ticket ticket) {
-		return repository.findByTicket(ticket);
-	}
-
-	@Override
-	public StatusHistory getLastStatusHistoryFromThisTicket(Ticket ticket) {
-		// On récupère tous les statuts du ticket, et on renvoit le dernier en date;
-		List<StatusHistory> statusHistories = repository.findByTicket(ticket);
-		if (statusHistories.size() > 0) {
-			// On trie la liste pour avoir le dernier statusHistory
-			Collections.sort(statusHistories, new Comparator<StatusHistory>() {
-				public int compare(StatusHistory sh1, StatusHistory sh2) {
-					if (sh1.getStatusDate() == null || sh2.getStatusDate() == null)
-						return 0;
-					return sh1.getStatusDate().compareTo(sh2.getStatusDate());
-				}
-			});
-			return statusHistories.get(statusHistories.size() - 1);
-		} else {
-			return null;
-		}
+		Optional<StatusHistory> statusHistory = repository.findById(logId);
+		return statusHistory.isPresent() ? statusHistory.get() : null;
 	}
 
 }
