ea4f2a5ad6950aa4756e3fac1ab273216341b3f2
Furvent <furventt@gmail.com>
2021-06-21 11:29:48 +0200

    Clean service Ticket

--- src/main/java/fr/eql/ticketing/service/TicketService.java
+++ src/main/java/fr/eql/ticketing/service/TicketService.java
@@ -1,20 +1,9 @@
 package fr.eql.ticketing.service;
 
-import java.util.List;
-
-import fr.eql.ticketing.entity.Group;
 import fr.eql.ticketing.entity.Ticket;
 
 public interface TicketService {
 	public Ticket save(Ticket ticket);
 
-	public List<Ticket> getAllTickets();
-
-	public List<Ticket> getTicketsWithGroup(Group group);
-
 	public Ticket getTicketById(Long ticketId);
-
-	public void delete(Ticket ticket);
-
-	public Ticket update(Ticket ticket);
 }
--- src/main/java/fr/eql/ticketing/service/TicketServiceImpl.java
+++ src/main/java/fr/eql/ticketing/service/TicketServiceImpl.java
@@ -1,18 +1,16 @@
 package fr.eql.ticketing.service;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
-import fr.eql.ticketing.entity.Group;
 import fr.eql.ticketing.entity.Ticket;
 import fr.eql.ticketing.repository.TicketRepository;
 
 @Service
 public class TicketServiceImpl implements TicketService {
 
-	TicketRepository repository;
+	private TicketRepository repository;
 
 	public TicketServiceImpl(TicketRepository repository) {
 		this.repository = repository;
@@ -24,29 +22,9 @@ public class TicketServiceImpl implements TicketService {
 	}
 
 	@Override
-	public List<Ticket> getAllTickets() {
-		return repository.findAll();
-	}
-
-	@Override
 	public Ticket getTicketById(Long ticketId) {
 		Optional<Ticket> ticket = repository.findById(ticketId);
 		return ticket.isPresent() ? ticket.get() : null;
-	}
-
-	@Override
-	public void delete(Ticket ticket) {
-		repository.delete(ticket);
-	}
-
-	@Override
-	public Ticket update(Ticket ticket) {
-		return repository.save(ticket);
-	}
-
-	@Override
-	public List<Ticket> getTicketsWithGroup(Group group) {
-		return repository.findByGroup(group);
 	}
 
 }
