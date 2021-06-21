6dc2aa26dfa150b456f8dd066b2684733195e1a1
Furvent <furventt@gmail.com>
2021-06-21 11:07:32 +0200

    Clean service Group

--- src/main/java/fr/eql/ticketing/service/GroupService.java
+++ src/main/java/fr/eql/ticketing/service/GroupService.java
@@ -1,19 +1,11 @@
 package fr.eql.ticketing.service;
 
-import java.util.List;
-
 import fr.eql.ticketing.entity.Group;
 
 public interface GroupService {
 	public Group save(Group group);
 
-	public List<Group> getAllGroups();
-
 	public Group getGroupById(Long groupId);
-
-	public void delete(Group group);
-
-	public Group update(Group group);
 
 	public boolean checkIfGroupExistWithThisId(long id);
 }
--- src/main/java/fr/eql/ticketing/service/GroupServiceImpl.java
+++ src/main/java/fr/eql/ticketing/service/GroupServiceImpl.java
@@ -1,6 +1,5 @@
 package fr.eql.ticketing.service;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
@@ -16,34 +15,15 @@ public class GroupServiceImpl implements GroupService {
 		this.repository = repository;
 	}
 
-	public void setRepository(GroupRepository repository) {
-		this.repository = repository;
-	}
-
 	@Override
 	public Group save(Group groupToAdd) {
 		return repository.save(groupToAdd);
 	}
 
 	@Override
-	public List<Group> getAllGroups() {
-		return repository.findAll();
-	}
-
-	@Override
 	public Group getGroupById(Long groupId) {
 		Optional<Group> group = repository.findById(groupId);
 		return group.isPresent() ? group.get() : null;
-	}
-
-	@Override
-	public void delete(Group group) {
-		repository.delete(group);
-	}
-
-	@Override
-	public Group update(Group group) {
-		return repository.save(group);
 	}
 
 	@Override
