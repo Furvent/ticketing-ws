08d6cc6bd884c2d72ee0ec7148378c891b302ccd
Furvent <furventt@gmail.com>
2021-06-21 11:25:44 +0200

    Clean service Task

--- src/main/java/fr/eql/ticketing/service/TaskService.java
+++ src/main/java/fr/eql/ticketing/service/TaskService.java
@@ -3,25 +3,12 @@ package fr.eql.ticketing.service;
 import java.util.List;
 
 import fr.eql.ticketing.entity.Task;
-import fr.eql.ticketing.entity.Ticket;
-import fr.eql.ticketing.entity.User;
 
 public interface TaskService {
 	public Task save(Task task);
 
 	public List<Task> saveAll(List<Task> tasks);
 
-	public List<Task> getAllTasks();
-
 	public Task getTaskById(Long taskId);
 
-	public void delete(Task task);
-
-	public Task update(Task task);
-
-	public List<Task> getTasksByTicket(Ticket ticket);
-
-	public List<Task> getTasksByUser(User user);
-
-	public List<Task> getTaskByUserAndTicket(User user, Ticket ticket);
 }
--- src/main/java/fr/eql/ticketing/service/TaskServiceImpl.java
+++ src/main/java/fr/eql/ticketing/service/TaskServiceImpl.java
@@ -1,12 +1,11 @@
 package fr.eql.ticketing.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import fr.eql.ticketing.entity.Task;
-import fr.eql.ticketing.entity.Ticket;
-import fr.eql.ticketing.entity.User;
 import fr.eql.ticketing.repository.TaskRepository;
 
 @Service
@@ -14,10 +13,6 @@ public class TaskServiceImpl implements TaskService {
 	TaskRepository repository;
 
 	public TaskServiceImpl(TaskRepository repository) {
-		this.repository = repository;
-	}
-
-	public void setRepository(TaskRepository repository) {
 		this.repository = repository;
 	}
 
@@ -32,38 +27,9 @@ public class TaskServiceImpl implements TaskService {
 	}
 
 	@Override
-	public List<Task> getAllTasks() {
-		return repository.findAll();
-	}
-
-	@Override
 	public Task getTaskById(Long taskId) {
-		return repository.findById(taskId).get();
-	}
-
-	@Override
-	public void delete(Task task) {
-		repository.delete(task);
-	}
-
-	@Override
-	public Task update(Task task) {
-		return repository.save(task);
-	}
-
-	@Override
-	public List<Task> getTasksByTicket(Ticket ticket) {
-		return repository.findByTicket(ticket);
-	}
-
-	@Override
-	public List<Task> getTasksByUser(User user) {
-		return repository.findByUser(user);
-	}
-
-	@Override
-	public List<Task> getTaskByUserAndTicket(User user, Ticket ticket) {
-		return repository.findByUserAndTicket(user, ticket);
+		Optional<Task> task = repository.findById(taskId);
+		return task.isPresent() ? task.get() : null;
 	}
 
 }
