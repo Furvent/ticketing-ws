21d9b8c5571d3e01df3a3e2ce88c9e32a8241427
Roch-Jacquot <30751823+Roch-Jacquot@users.noreply.github.com>
2021-07-30 16:03:12 +0200

    Update GroupDashboardRestControllerIT.java

--- src/test/java/fr/eql/ticketing/restController/GroupDashboardRestControllerIT.java
+++ src/test/java/fr/eql/ticketing/restController/GroupDashboardRestControllerIT.java
@@ -1,6 +1,6 @@
 package fr.eql.ticketing.restController;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.security.cert.CollectionCertStoreParameters;
 import java.time.LocalDateTime;
